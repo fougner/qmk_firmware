1c4a234485081bb74bc11ca0b932f3fa02a5aef8
Alexander Fougner <fougner89@gmail.com>
2018-12-03 19:09:30 +0100

    xd87: add capslock led support

--- keyboards/xd87/xd87.c
+++ keyboards/xd87/xd87.c
@@ -18,7 +18,7 @@
 void matrix_init_kb(void) {
   // put your keyboard start-up code here
   // runs once when the firmware starts up
-
+  led_init_ports();
   matrix_init_user();
 }
 
@@ -39,5 +39,16 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
 void led_set_kb(uint8_t usb_led) {
   // put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
 
+    if (usb_led & (1<<USB_LED_CAPS_LOCK))
+        PORTE &= ~(1<<2);
+    else
+        PORTE |=  (1<<2);
+
+
   led_set_user(usb_led);
+}
+
+void led_init_ports(void) {
+    DDRE |= (1<<2);
+    PORTE |= (1<<2);
 }
